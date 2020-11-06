1aa70479f63226ce53528c1311d241136b5ba2b5
vlad <hoo1y@yandex.ru>
2020-11-06 23:29:44 +0300

    finish 1.3

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -3,15 +3,15 @@ import AuthContext from './AuthContext';
 
 const UserInfo = () => (
     <AuthContext.Consumer>
-    {
-        user => (
-            <div class='w3-right w3-row' >
+        {
+            user => (
+                <div class='w3-right w3-row'>
                     <div class='w3-col w3-padding'>
-                        <img width='40px' src={user.avatar} /> {user.firstName} {user.lastName}
+                        <img width='40px' src={user.avatar}/> {user.firstName} {user.lastName}
                     </div>
-            </div>
-        )
-}
+                </div>
+            )
+        }
     </AuthContext.Consumer>
 );
 
