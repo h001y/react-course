03fca3b2285ea7b6f9132afe7c8e969ec531fcfc
hoo1y <hoo1y@yandex.ru>
2021-01-21 12:27:49 +0300

    pr fixes

--- src/bookCard/BookName.js
+++ src/bookCard/BookName.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-const BookName = (props) => {
-    const { name } = props;
+const BookName = ({ name }) => {
 
     return (
-        <>
             <div className='w3-row'>
                 <div className='w3-col w3-padding w3-xxlarge left'>{name}</div>
             </div>
-        </>
     );
 }
 
